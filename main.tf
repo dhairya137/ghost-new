776c014474e1e1a602f97f56a6e5551d3bba946c
Dhairya Patel <dhairya00798@gmail.com>
2022-05-11 18:21:42 +0530

    VM Auto

---
+++ main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = "ap-south-1"
+  shared_credentials_file = "/home/dhairya/.aws/credentials"
+}
+
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "vm_name" {}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = [var.my_ip]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-sg"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_eip" "example" {
+  instance = aws_instance.myapp-server.id
+  vpc      = true
+}
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  # subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  # availability_zone      = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name                    = "tf-key-pair"
+
+  tags = {
+    Name : "${var.vm_name}-${var.env_prefix}-server"
+  }
+}
+
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+output "eip" {
+  value = aws_eip.example.public_ip
+}
--- setup.sh
+++ setup.sh
@@ -41,5 +41,23 @@ if [ "$1" == "setup" ]; then
   docker-compose -f docker-compose.prod.yml up -d --build 
   #  echo 'Access your ghost: https://'$2;
   # echo 'Access your ghost admin page: https://'$2'/ghost';
+fi
 
+
+if [ "$1" == vminit ]; then
+  echo "Creating VM..."
+
+  terraform apply -auto-approve
+  terraform output -json eip > ip.txt
+  echo "VM Created Successfully"
+fi
+
+if [ "$1" == install ]; then
+  echo "Installing Ghost..."
+  echo "Pass domain name and email id at the time of executing script"
+  cp remote.txt remoteexec.tf
+  terraform apply -auto-approve
+  # terraform apply -auto-approve -var domain_name="$2" 
+  echo "Ghost installed Successfully"
+  # > remoteexec.tf 
 fi
